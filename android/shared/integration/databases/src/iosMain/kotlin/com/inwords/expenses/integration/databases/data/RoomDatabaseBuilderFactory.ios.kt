0f5ed4d426d8075c8e08ce27693ef33ce84ad0ca
Vasilii Shumilov <vshumilov@yandex-team.ru>
2026-04-02 18:50:19 +0200

    Fix iOS real device runtime crash

--- android/shared/integration/databases/src/iosMain/kotlin/com/inwords/expenses/integration/databases/data/RoomDatabaseBuilderFactory.ios.kt
+++ android/shared/integration/databases/src/iosMain/kotlin/com/inwords/expenses/integration/databases/data/RoomDatabaseBuilderFactory.ios.kt
@@ -2,12 +2,24 @@ package com.inwords.expenses.integration.databases.data
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+import platform.Foundation.NSURL
 
 internal actual class RoomDatabaseBuilderFactory {
 
+    @OptIn(ExperimentalForeignApi::class)
     actual fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
-        val dbFilePath = NSHomeDirectory() + "/app_db.db"
+        val fileManager = NSFileManager.defaultManager
+        val documentDirectory = fileManager.URLsForDirectory(
+            directory = NSDocumentDirectory,
+            inDomains = NSUserDomainMask,
+        ).first() as NSURL
+        val dbDirectory = documentDirectory.path!! + "/databases"
+        fileManager.createDirectoryAtPath(dbDirectory, withIntermediateDirectories = true, attributes = null, error = null)
+        val dbFilePath = "$dbDirectory/app_db.db"
         return Room.databaseBuilder<AppDatabase>(
             name = dbFilePath,
         )
